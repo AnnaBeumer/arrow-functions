8c9a3d6fd55a3c51b55242ab0415a46d3a7ab7b5
Anna Beumer <anna.m.beumer@gmail.com>
2020-12-22 15:52:53 +0100

    add index and script

---
+++ script.js
@@ -0,0 +1,21 @@
+// Deel A
+const ikRockArrowFunctions = () =>
+  console.log("Joe, ik rock de arrow functions");
+
+ikRockArrowFunctions();
+
+// Deel B
+const fivePlusSeven = () => 5 + 7;
+fivePlusSeven();
+
+// Deel C
+const myFunction = (a, b) => a + b;
+myFunction(2, 3);
+
+// Deel D
+const addFive = (a) => a + 5;
+addFive(5);
+
+// Deel E
+const createObject = () => ({ greeting: "hoi" });
+createObject();
